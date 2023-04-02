d5b67dbb6988ae02588879c632c5e08ca06d09c0
Noah <noahcorona231@gmail.com>
2023-04-02 15:11:29 -0700

    Add camera tracking and player speed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use bevy::{prelude::*};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(input)
+        .add_system(move_player)
+        .add_system(move_camera)
         .run();
 }
 
 #[derive(Default, Component)]
 struct Player;
+#[derive(Default, Component)]
+struct Hostile;
+#[derive(Default, Component)]
+struct Speed(f64);
 
 fn setup(
     mut commands: Commands,
@@ -18,7 +23,7 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    // Rectangle
+    // Player
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
@@ -29,14 +34,43 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
             ..default()
         })
-        .insert(Player);
+        .insert((Player, Speed(2.5)));
+
+    // Hostile
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::RED,
+                custom_size: Some(Vec2::new(30.0, 30.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+            ..default()
+        })
+        .insert(Hostile);
 }
 
-fn input(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Player>>) {
-    let mut transform = query.single_mut();
+/// Move camera to player position
+fn move_camera(
+    mut transforms: Query<&mut Transform>,
+    player: Query<Entity, With<Player>>,
+    camera: Query<Entity, With<Camera>>,
+) {
+    let player_transform = *transforms.get(player.single()).unwrap();
+    let mut camera_transform = transforms.get_mut(camera.single()).unwrap();
 
-    transform.translation.x -= keyboard_input.pressed(KeyCode::A) as i32 as f32;
-    transform.translation.x += keyboard_input.pressed(KeyCode::D) as i32 as f32;
-    transform.translation.y += keyboard_input.pressed(KeyCode::W) as i32 as f32;
-    transform.translation.y -= keyboard_input.pressed(KeyCode::S) as i32 as f32;
+    camera_transform.translation = player_transform.translation;
+}
+
+/// Move player with WASD
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &Speed), With<Player>>,
+) {
+    let (mut transform, speed) = query.single_mut();
+
+    transform.translation.x -= keyboard_input.pressed(KeyCode::A) as i32 as f32 * speed.0 as f32;
+    transform.translation.x += keyboard_input.pressed(KeyCode::D) as i32 as f32 * speed.0 as f32;
+    transform.translation.y += keyboard_input.pressed(KeyCode::W) as i32 as f32 * speed.0 as f32;
+    transform.translation.y -= keyboard_input.pressed(KeyCode::S) as i32 as f32 * speed.0 as f32;
 }
