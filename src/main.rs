37e28d3ae9a4816a3baebd430020621469725d97
Noah <noahcorona231@gmail.com>
2023-04-02 14:58:59 -0700

    Add movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::{prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(input)
+        .run();
+}
+
+#[derive(Default, Component)]
+struct Player;
+
+fn setup(
+    mut commands: Commands,
+    _meshes: ResMut<Assets<Mesh>>,
+    _materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Rectangle
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(30.0, 30.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+            ..default()
+        })
+        .insert(Player);
+}
+
+fn input(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Player>>) {
+    let mut transform = query.single_mut();
+
+    transform.translation.x -= keyboard_input.pressed(KeyCode::A) as i32 as f32;
+    transform.translation.x += keyboard_input.pressed(KeyCode::D) as i32 as f32;
+    transform.translation.y += keyboard_input.pressed(KeyCode::W) as i32 as f32;
+    transform.translation.y -= keyboard_input.pressed(KeyCode::S) as i32 as f32;
 }
